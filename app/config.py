25e17966cc489cd54f27ac0217668811a66435e3
adityavkulkarni <adityavkulkarni0999@gmail.com>
2025-07-13 11:07:49 -0500

    Modified config

--- app/config.py
+++ app/config.py
@@ -7,13 +7,13 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///document_manager.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PARENT_FOLDER = os.getenv('PARENT_FOLDER', 'uploads')
-    STATIC_FOLDER = os.path.join(os.getcwd(), "app", "static")
-    TMP_FOLDER = os.path.join(os.getcwd(), os.getenv('TMP_FOLDER', 'tmp'))
+    PARENT__DIRECTORY = os.getenv('PARENT_DIRECTORY', 'uploads')
+    STATIC__DIRECTORY = os.path.join(os.getcwd(), "app", "static")
+    TMP__DIRECTORY = os.path.join(os.getcwd(), os.getenv('TMP_DIRECTORY', 'tmp'))
     LOG_DIRECTORY = os.path.join(os.getcwd(), 'logs')
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB limit
     ALLOWED_EXTENSIONS = {'pdf', 'jpg', 'jpeg', 'png'}
-    HADOOP = False
+    HADOOP = True if os.getenv('FILE_SYSTEM', 'local') == 'hadoop' else False
     HADOOP_NAMENODE_URL = os.getenv('HADOOP_NAMENODE_URL', None)
     HADOOP_USERNAME = os.getenv('HADOOP_USERNAME', None)
     if HADOOP_USERNAME is None or HADOOP_NAMENODE_URL is None:
